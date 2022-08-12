1f4f7ca79c2cfb9e5eb4745d652e3d641fc490c6
Isaías Oliveira dos Reis <isaiasodr@gmail.com>
2022-08-12 14:08:09 -0300

    modificação das crendenciais do banco de dados para deploy

--- app/index.js
+++ app/index.js
@@ -1,44 +1,37 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
+const express = require("express");
+const exphbs = require("express-handlebars");
 
-const app = express()
+const app = express();
 
-const conn = require('./db/conn')
+const conn = require("./db/conn");
 
-const Task = require('./models/Task')
+const Task = require("./models/Task");
 
-const TaskRoutes = require('./routes/tasksroute')
+const TaskRoutes = require("./routes/tasksroute");
 
+app.engine("handlebars", exphbs.engine());
 
-app.engine('handlebars', exphbs.engine())
-
-app.set('view engine', 'handlebars')
-
+app.set("view engine", "handlebars");
 
 app.use(
-    express.urlencoded({
-        extends: true
-    })
-)
+  express.urlencoded({
+    extends: true,
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/tasks', TaskRoutes)
+app.use("/tasks", TaskRoutes);
 
-app.get('/',(req,res)=>{
-    res.render("tasks/all")
-})
+app.get("/", (req, res) => {
+  res.render("tasks/all");
+});
 
-app.use(express.static('public'))
-
-
-
-
-
+app.use(express.static("public"));
 
 conn
-    .sync()
-    .then(() => {
-        app.listen(5000)
-    })
-    .catch((err) => console.log(err))
+  .sync({ force: true })
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
